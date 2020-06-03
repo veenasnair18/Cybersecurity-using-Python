d15ce83b67aa2609346ba71ef63736d420059352
Veena S Kumar <38048590+veenasnair18@users.noreply.github.com>
2020-06-03 15:18:29 +0530

    Add files via upload

---
+++ Faker.py
@@ -0,0 +1,46 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from faker import Faker
+>>> fake=Faker()
+>>> print(fake.email())
+rebecca34@king-cunningham.com
+>>> print(fake.country())
+Kyrgyz Republic
+>>> print(fake.name())
+Charles Bell
+>>> print(fake.text())
+Run case and rather suggest focus. Discuss because social. Prevent themselves main artist else.
+>>> print(fake.url())
+http://www.dixon-jones.net/
+>>> print(fake.address())
+22298 Shaffer Hill
+New Carolyn, KY 52109
+>>> print(fake.language_code())
+en
+>>> print(fake.latitude())
+12.827589
+>>> print(fake.longitude())
+-124.227016
+>>> print(fake.csv())
+"Brian Mora","Unit 7720 Box 8895
+DPO AP 83616"
+"Duane Lee","38543 Cain Run
+South Janeton, MA 30221"
+"Sharon Waters","4564 Wolf Shore Suite 048
+Philipstad, CT 70168"
+"Lawrence Dawson","465 Pamela Groves
+West Christopherstad, SC 25419"
+"Jordan Berg","57133 Pearson Cliff
+New Kimberly, SD 12402"
+"Carla Reilly","366 Hunter Plaza
+South Markport, MI 69985"
+"Trevor Wagner","165 Holland Valley Suite 021
+Ryanchester, AZ 93601"
+"Robert Fischer","4949 Hickman Unions Suite 131
+Antoniochester, WV 94406"
+"Ashley Greer","0967 Danielle Prairie
+Ryanburgh, ID 20492"
+"Daniel Ibarra","440 Anderson Canyon
+Edwardton, DC 04090"
+
+>>> 
